47cad0ca3e2ddd6d2870d5ea2aa7698a9cf5840f
vansh-bothra <vanshbothra@gmail.com>
2026-02-02 16:41:17 +0530

    fix: view engine path

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ const websiteRoutes = require("./routes/websiteRoutes.js");
 // Set view engine to EJS
 app.set("view engine", "ejs");
 
+// Set the views directory
+app.set("views", path.join(__dirname, "views"));
+
 // Serve static files from the "public" directory
 app.use(
   "/",
